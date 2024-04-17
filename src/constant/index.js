ab55c4e58e635c1e24ec10fa38f10a8e6a7116ef
Manish Gusain <manishgusain66@gmail.com>
2024-04-17 19:55:20 +0530

    section and ui changes

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,50 +1,81 @@
 export const experienceData = [
     {
-      companyName: "Shyam Health Care",
-      duration: "08/20 – 12/21",
-      position: "SDE-1",
-      tasks: [
-        "Assisted in the development and maintenance of web applications using React.",
-        "Participated in code reviews and collaborated with senior developers on project tasks.",
-        "Resolved bugs and issues reported by end-users, improving overall software quality.",
-        "Collaborated with cross-functional teams to troubleshoot and resolve software defects.",
-        "Integrated RESTful APIs for data retrieval and seamless user experience."
-      ]
+        companyName: "Shyam Health Care",
+        duration: "08/20 – 12/21",
+        position: "SDE-1",
+        tasks: [
+            "Assisted in the development and maintenance of web applications using React.",
+            "Participated in code reviews and collaborated with senior developers on project tasks.",
+            "Resolved bugs and issues reported by end-users, improving overall software quality.",
+            "Collaborated with cross-functional teams to troubleshoot and resolve software defects.",
+            "Integrated RESTful APIs for data retrieval and seamless user experience."
+        ]
     },
     {
-      companyName: "isPAY",
-      duration: "01/22 – 06/22",
-      position: "SDE-1",
-      tasks: [
-        "Developed a fully functional application from the ground up by coding all screens, implementing Rest APIs and websockets and created reusable components.",
-        "Implemented user authentication using aws-cognito.",
-        "Throughout the application lifecycle, I effectively maintained and released bug fixes and implemented various features and functionalities.",
-        "Also leveraged ReactJS to build an admin portal and gained exposure to various AWS services such as S3, Api-Gateway, Cognito, Amplify, Lambda, Codebuild, and CloudWatch.",
-        "Utilized Firebase for user registration and a"
-      ]
+        companyName: "isPAY",
+        duration: "01/22 – 06/22",
+        position: "SDE-1",
+        tasks: [
+            "Developed a fully functional application from the ground up by coding all screens, implementing Rest APIs and websockets and created reusable components.",
+            "Implemented user authentication using aws-cognito.",
+            "Throughout the application lifecycle, I effectively maintained and released bug fixes and implemented various features and functionalities.",
+            "Also leveraged ReactJS to build an admin portal and gained exposure to various AWS services such as S3, Api-Gateway, Cognito, Amplify, Lambda, Codebuild, and CloudWatch.",
+            "Utilized Firebase for user registration and a"
+        ]
     },
     {
-      companyName: "Embifi",
-      duration: "06/22 – 07/23",
-      position: "Full Stack Engineer",
-      tasks: [
-        "Developed responsive and interactive web and mobile applications using ReactJS and React Native.",
-        "Built reusable and maintainable UI components to improve code efficiency and scalability.",
-        "Worked closely with the design team to ensure the applications were visually appealing and user-friendly.",
-        "Collaborated with the back-end team to integrate the front-end code with APIs.",
-        "Conducted code reviews, identified performance bottlenecks, and optimized the code for better user experience.",
-        "Experienced in AWS tools such as S3, code-commit, api-gateway, amplify, lambda."
-      ]
+        companyName: "Embifi",
+        duration: "06/22 – 07/23",
+        position: "Full Stack Engineer",
+        tasks: [
+            "Developed responsive and interactive web and mobile applications using ReactJS and React Native.",
+            "Built reusable and maintainable UI components to improve code efficiency and scalability.",
+            "Worked closely with the design team to ensure the applications were visually appealing and user-friendly.",
+            "Collaborated with the back-end team to integrate the front-end code with APIs.",
+            "Conducted code reviews, identified performance bottlenecks, and optimized the code for better user experience.",
+            "Experienced in AWS tools such as S3, code-commit, api-gateway, amplify, lambda."
+        ]
     },
     {
-      companyName: "Freelancer",
-      duration: "08/23 – 10/23",
-      position: "Freelancer",
-      tasks: [
-        "Freelanced for a stealth mode startup. [ ReactJs, React Native ]"
-      ]
+        companyName: "Freelancer",
+        duration: "08/23 – 10/23",
+        position: "Freelancer",
+        tasks: [
+            "Freelanced for a stealth mode startup. [ ReactJs, React Native ]"
+        ]
     }
-  ];
+];
 
 
-  export const aboutData = "Skilled Software Engineer with extensive experience working in an agile startup environment. Proficient in developing scalable, responsive software applications and integrating them with back-end services building products from scratch and collaborating with cross-functional teams.";
+export const aboutData = "Skilled Software Engineer with extensive experience working in an agile startup environment. Proficient in developing scalable, responsive software applications and integrating them with back-end services building products from scratch and collaborating with cross-functional teams.";
+
+export const projectData = [
+    {
+        title: "Stock Average Calculator",
+        description: "Developed a mobile application using React Native that has been successfully published on the Google Play Store and has garnered over 50+ downloads and has an impressive rating of 4.9.",
+        downloadLink: "https://play.google.com/store/apps/details?id=com.sac.android"
+    },
+    {
+        title: "Tic Tac Toe",
+        description: "Developed a mobile application based on the classic game of Tic Tac Toe. The app provides a simple and intuitive interface for users to play against each other. It features customizable player names, an interactive game board, and victory detection. Published on the Google Play Store with positive user reviews and ratings.",
+        downloadLink: "https://play.google.com/store/apps/details?id=com.ttk.android"
+    },
+    {
+        title: "Embifi Lending App for EV Dealers",
+        description: "A lending application tailored for electric vehicle (EV) dealers, enabling them to offer financing options to their customers.",
+        downloadLink: "https://play.google.com/store/apps/details?id=com.e_rick"
+    },
+    {
+        title: "React Native Doughnut Chart",
+        description: "This module simplifies the process of incorporating sleek and interactive doughnut charts into your projects. Published on NPM",
+        downloadLink: "https://www.npmjs.com/package/react-native-doughnut-chart"
+    }
+];
+
+export const sectionOptions = [
+    'About', 
+    'Experience', 
+    // 'Skills', 
+    'Projects',
+    // 'Get In Touch'
+];
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import { aboutData, experienceData } from '../constant';
+import { aboutData, experienceData, projectData, sectionOptions } from '../constant';
 
 const AboutContent = () => {
     return (
         <div className='about-container'>
-            <h2>{aboutData}</h2>
+            <h1 className='name'>{'Manish Gusain'}</h1>
+            <h3>{aboutData}</h3>
+            <h3 className='about-sub'>{'Experienced In'}</h3>
+            <h3 className='about-sub'>{'Get In Touch'}</h3>
+            <h3>{'manishgusain66@gmail.com'}</h3>
+            <br/>
+            <h3>{'9899809880'}</h3>
         </div>
     );
 };
@@ -20,16 +26,18 @@ const SkillsContent = () => {
     );
 };
 
-const ProjectContent = () => {
+const ProjectContent = ({ title, description, downloadLink }) => {
     return (
-        <div>
-            <h2>Skills</h2>
-            <p>JavaScript | React JS | React Native | HTML |
-                CSS | AWS | MySQL | Redux | Bootstrap | Git
-                | Python | Rest API's</p>
+        <div className='project-container'>
+            <h2 className="experience-title">{title}</h2>
+            <p className="position-title">{description}</p>
+            <div className="dl-btn-container">
+                <a href={downloadLink} className='dl-btn' target="_blank">Download Link</a>
+            </div>
         </div>
     );
 };
+
 
 const Experience = ({ companyName, duration, position, tasks }) => (
     <div className="experience-container">
@@ -44,7 +52,7 @@ const Experience = ({ companyName, duration, position, tasks }) => (
 );
 
 export default function Homepage() {
-    const [selectedOption, setSelectedOption] = useState('Experience');
+    const [selectedOption, setSelectedOption] = useState('About');
 
     const renderContent = () => {
         switch (selectedOption) {
@@ -65,20 +73,34 @@ export default function Homepage() {
             case 'Skills':
                 return <SkillsContent />;
             case 'Projects':
-                return <ProjectContent />;
+                return (
+                    projectData.map((project, index) => (
+                        <ProjectContent
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            downloadLink={project.downloadLink}
+                        />
+                    ))
+                );
             default:
                 return null;
         }
     };
 
+    const handleSelect = (label) => setSelectedOption(label);
+
     return (
         <div className="container">
             <div className="section">
                 <div>
-                    <p className='sec-label' onClick={() => setSelectedOption('Experience')}>Experience</p>
-                    <p className='sec-label' onClick={() => setSelectedOption('About')}>About</p>
-                    <p className='sec-label' onClick={() => setSelectedOption('Skills')}>Skills</p>
-                    <p className='sec-label' onClick={() => setSelectedOption('Projects')}>Projects</p>
+                    {
+                        sectionOptions.map((option, index) => {
+                            return (
+                                <p className='sec-label' key={index} onClick={() => handleSelect(option)}>{option}</p>
+                            );
+                        })
+                    }
                 </div>
             </div>
             <div className="content">
